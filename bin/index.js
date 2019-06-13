0fdae1dac1a1f85a6c030e0ae89719abbc4df9d8
Fredrik Söderquist <fregu808@gmail.com>
2019-06-13 13:16:11 +0200

    in  progress backup

--- bin/index.js
+++ bin/index.js
@@ -1,64 +1,72 @@
 #!/usr/bin/env node
 
 // TODO: Implement process management using PM2 API (http://pm2.keymetrics.io/docs/usage/pm2-api/)  //
-const npx = require("node-npx");
-const path = require("path");
-const build = require("./build");
-const generators = require("../generators");
-const createServer = require("../lib/server");
-const config = require("../lib/config");
+const npx = require('node-npx')
+const path = require('path')
+const build = require('./build')
+const generators = require('../generators')
+const createServer = require('../lib/server')
+const config = require('../lib/config')
+const parser = require('../lib/parser/parse')
 
 if (process.argv.length > 2) {
-  const dir = process.cwd();
-  const command = process.argv[2];
+  const dir = process.cwd()
+  const command = process.argv[2]
   const specifier =
-    process.argv[3] && !process.argv[3].match(/^-/) && process.argv[3];
+    process.argv[3] && !process.argv[3].match(/^-/) && process.argv[3]
   const flags = process.argv
     .filter(arg => arg.match(/^-/))
-    .map(arg => arg.replace(/^--?/, ""));
+    .map(arg => arg.replace(/^--?/, ''))
 
   switch (command) {
-    case "init":
-      console.log("Setting up new Tedious project setup");
-      generators(config).init();
+    case 'init':
+      console.log('Setting up new Tedious project setup')
+      generators(config).init()
 
-      break;
-    case "cms":
-      console.log("Setting up new Strapi.io CMS");
-      generators(config).cms();
+      break
+    case 'cms':
+      console.log('Setting up new Strapi.io CMS')
+      generators(config).cms()
 
-      break;
-    case "build":
-      mode = flags.includes("dev") ? "development" : "production";
-      build("build", {
+      break
+    case 'build':
+      mode = flags.includes('dev') ? 'development' : 'production'
+      build('build', {
         mode,
         watch: false,
         styleguide: false,
         server: false
-      });
-      break;
-    case "styleguide":
-      const styleguide = require("../lib/styleguide")(config);
-      if (flags.includes("build")) {
-        styleguide.build();
+      })
+      break
+    case 'styleguide':
+      const styleguide = require('../lib/styleguide')(config)
+      if (flags.includes('build')) {
+        styleguide.build()
       } else {
-        styleguide.serve();
+        styleguide.serve()
       }
-      break;
-    case "start":
+      break
+    case 'start':
       mode =
-        flags.includes("prod") || flags.includes("production")
-          ? "production"
-          : "development";
+        flags.includes('prod') || flags.includes('production')
+          ? 'production'
+          : 'development'
       // forEach entry create server and start on separate port?
-      createServer({ ...config, mode });
-      break;
-    case "test":
+      createServer({ ...config, mode })
+      break
+    case 'test':
       console.log(
-        "Not yet implemented, would be running your tests, lints and type testers right now otherwise"
-      );
-      break;
+        'Not yet implemented, would be running your tests, lints and type testers right now otherwise'
+      )
+      break
+    case 'parse':
+      const fs = require('fs')
+      fs.writeFileSync(
+        `${dir}/fullConfig.json`,
+        JSON.stringify(parser(dir), null, 2)
+      )
+      break
   }
 } else {
-  console.log("available commands...");
+  console.log('available commands...')
 }
