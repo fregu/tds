2932d8576e638246664bd3345cc015f2c32affe7
Fredrik Söderquist <fredrik.soderquist@jayway.com>
2019-08-08 14:02:29 +0200

    using dev runner with createServer to handle server same between dev and prod.

--- bin/index.js
+++ bin/index.js
@@ -1,11 +1,8 @@
 #!/usr/bin/env node
 
 // TODO: Implement process management using PM2 API (http://pm2.keymetrics.io/docs/usage/pm2-api/)  //
-const npx = require('node-npx')
-const path = require('path')
 const builder = require('../lib/build')
 const generators = require('../generators')
-// const runner = require('../lib/server/runner')
 const createServer = require('../lib/server')
 const runner = require('../lib/server/runner')
 const config = require('../lib/config')
@@ -15,11 +12,16 @@ const parser = require('../lib/parser/parse')
 if (process.argv.length > 2) {
   const dir = process.cwd()
   const command = process.argv[2]
-  const specifier =
-    process.argv[3] && !process.argv[3].match(/^-/) && process.argv[3]
+  // const specifier =
+  //   process.argv[3] && !process.argv[3].match(/^-/) && process.argv[3]
   const flags = process.argv
     .filter(arg => arg.match(/^-/))
     .map(arg => arg.replace(/^--?/, ''))
+
+  const mode =
+    flags.includes('prod') || flags.includes('production')
+      ? 'production'
+      : 'development'
 
   switch (command) {
     case 'init':
@@ -27,13 +29,8 @@ if (process.argv.length > 2) {
       generators(config).init()
 
       break
-    case 'cms':
-      console.log('Setting up new Strapi.io CMS')
-      generators(config).cms()
 
-      break
     case 'build':
-      mode = flags.includes('dev') ? 'development' : 'production'
       builder()
         .server()
         .run(() => console.log('build done'))
@@ -47,17 +44,9 @@ if (process.argv.length > 2) {
       }
       break
     case 'start':
-      mode =
-        flags.includes('prod') || flags.includes('production')
-          ? 'production'
-          : 'development'
-      createServer({ ...config, mode })
+      runner({ ...config, mode })
       break
     case 'runner':
-      mode =
-        flags.includes('prod') || flags.includes('production')
-          ? 'production'
-          : 'development'
       runner({ ...config, mode })
       break
     case 'test':
